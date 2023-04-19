17807ae7118271ea996e67bd1aa45047689a1217
Shinpei Aoki <shimpei.aoki@makuake.co.jp>
2023-04-19 15:35:16 +0900

    環境変数に入っているシークレット出力を削除

--- test_app/fuel/app/tasks/example.php
+++ test_app/fuel/app/tasks/example.php
@@ -4,10 +4,9 @@ namespace Fuel\Tasks;
 class Example
 {
 
-	public function run()
-	{
-        echo getenv('WRITER_DB_PASSWORD') . "\n";
-        $users = \Model_User::find('all');
-        print_r($users);
+        public function run()
+        {
+                $users = \Model_User::find('all');
+                print_r($users);
 	}
 }
